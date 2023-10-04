75de13d1e079ecb98bb430585da374460487f30a
ahmedalaa404 <az.ahmed.alaa@gmail.com>
2023-10-04 09:24:42 +0300

    Add project files.

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿
+
+namespace GameZone.Data
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> Options):base(Options)
+        {
+            
+        }
+    }
+}
